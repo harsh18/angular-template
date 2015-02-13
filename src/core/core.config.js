57ccb19def2608968f1e3b7ddc5d13ac6cc84706
harsh18 <harsh.lamba181189@gmail.com>
2015-02-13 16:36:09 +0530

    urls entry

--- src/core/core.config.js
+++ src/core/core.config.js
@@ -13,8 +13,10 @@
 	appConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
 	function appConfig($stateProvider, $urlRouterProvider){
+		console.log($stateProvider);
+		console.log($urlRouterProvider);
 		//Otherwise Route - takes the url path
-		$urlRouterProvider.otherwise("/login");
+		$urlRouterProvider.otherwise("/dashboard");
 
 		//Otherwise Rule - takes the function
 		$urlRouterProvider.rule(otherWiseUrlCheck);
@@ -24,22 +26,20 @@
 		function otherWiseUrlCheck($injector, $location){
 			//here we can check where user will be redirected - login or dashboard
 			//$location.url - used to redirect to some states
+			var url = '/login';
+			console.log($location.url(url));
 		}
 
 		$stateProvider
 
 			.state('login', {
-				url : '/login/login.html',
-				controller : function($scope){
-					console.log('login page');
-				}
+				url : '/login',
+				templateUrl : '/login/login.html',
 			})		
 
 			.state('dashboard', {
-				url : '/dashboard/dashboard.html',
-				controller : function($scope){
-					console.log('dashboard page');
-				}
+				url : '/dashboard',
+				templateUrl : '/dashboard/dashboard.html'
 			})
 	}
 
