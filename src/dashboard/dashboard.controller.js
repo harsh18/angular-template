defc0f79be2b2e853567081937b89fc4e2c1ce7e
harsh18 <harsh.lamba181189@gmail.com>
2015-02-19 19:08:06 +0530

    ErrorInterceptor

--- src/dashboard/dashboard.controller.js
+++ src/dashboard/dashboard.controller.js
@@ -87,7 +87,7 @@
 
 			function getOrders(){
 				//Calling Order Service and store in model orders
-				orderService.getInputDetails('order').then(getOrderDetails, errorGetOrder);
+				orderService.getInputDetails('orders').then(getOrderDetails, errorGetOrder);
 				function getOrderDetails(data){
 					console.log(data.status);
 					var dataOrder = data;
